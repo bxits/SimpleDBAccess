e85ab43267182958ffe0fc6aedc2314f0523956b
bxits <ruediger.berg@its-stuttgart.de>
2021-03-19 16:18:18 +0100

    Projektdateien hinzufügen.

---
+++ SimpleDBAccess/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+//Vorher die dll MySql.Data.dll zu den Verweisen hinzufügen.
+//dann kann man auf den Namespace verweisen.
+//Quelle: https://dev.mysql.com/downloads/connector/net/
+using MySql.Data.MySqlClient;
+
+namespace SimpleDBAccess
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInsert_Click(object sender, RoutedEventArgs e)
+        {
+            ///Um diese Klassen benutzen zu können, müssen Sie den Namespace MySql.Data in den
+            ///using-Direktiven (siehe oben) hinzufügen. 
+            ///
+            ///Jetzt können wir als Erstes eine Verbindung zur Datenbank herstellen. In einem sog.
+            ///Connection-String geben wir die nötigen Verbindungsinformationen an:
+            ///- Server-Adresse/IP
+            ///- Name der Datenbank
+            ///- User, der auf die Datenbank zugreifen darf und
+            ///- Passwort (Sicherheitskriterien beachten!)
+            ///Natürlich müssen Datenbank und User auf der Datenbank existieren.
+            MySqlConnection con = new MySqlConnection(@"SERVER = 192.168.7.7;DATABASE=demodb;UID=user1;PASSWORD=pwd1;");
+            
+            //Nachdem die Zugangsdaten gesetzt wurden, können wir den "Kanal" zur Datenbank öffnen.
+            con.Open();  //Jetzt greift unser Programm über das Netzwerk/lokal auf die Datenbank zu.
+
+            ///Nun können wir einen SQL-Befehl an die DB senden, der Daten in eine Tabelle einfügt. 
+            ///Dies geht im einfachsten Fall über SQL-Befehle als String.
+            string insert = "INSERT INTO tbldemo VALUES('das ist ein Eintrag')";
+            //Damit der MySQL-Server das SQL-Statement verarbeiten kann, müssen wir es in einen MySqlCommand-Objekt umwandeln.
+            //Dafür erstellen wir ein MySqlCommand-Objekt mit new...
+            MySqlCommand cmd = new MySqlCommand();
+            //..und fügen die nötigen Informationen hinzu:
+            cmd.CommandText = insert;
+            cmd.Connection = con; //Damit weiß der Command, welche Verbindung er zum DB-Server verwenden soll.
+
+            //Mit ExecuteNonQuery() führen wir den Command auf der DB aus.
+            cmd.ExecuteNonQuery();
+
+            //So, wenn alles funktioniert hat, solle nun in der Datenbank ein Eintrag in der Tabelle tbldemo vorhanden sein.
+            //Überprüfen Sie dies....
+        }
+
+        private void btnGet_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SimpleDBAccess/Readme.txt
@@ -0,0 +1,4 @@
+﻿Diese Programmierbeispiel zeigt den einfachsten Weg, um auf Daten in einer MySQL-Datenbank zuzugreifen.
+Um die Sicht auf die wesentlichen Funktionsmerkmale zu fokussieren, wurden Aspekte der Sicherheit (SQL-Injection,
+Passwortverschüllelung) vernachlässigt, genauso wie das Abfangen von Fehlern (try/catch) und
+eine mehrschichtige Architektur.
